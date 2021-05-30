87f99fd7dc7fa73e74f8ca58e9e4bd4fb69cb68c
nicocamut <81877654+nicocamut@users.noreply.github.com>
2021-05-30 22:09:49 +0200

    Add files via upload

---
+++ js/audio.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('.play-button');
+
+let sound;
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		sound && sound.fade(1, 0, 500);
+		setTimeout(() => {
+			sound = new Howl({ src: [`audio/${button.id}.mp3`] });
+			sound.play();
+		}, 500);
+	});
+});
